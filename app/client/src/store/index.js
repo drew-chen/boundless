978e4c8bb055faa2ed85633fa6f3ebe4373acbe1
Andrew Chen <Andrew.Chen@windriver.com>
2020-07-16 18:23:24 -0700

    Add basic draggable form creation using Vue.Draggable
    Signed-off-by: Andrew Chen <Andrew.Chen@windriver.com>

--- app/client/src/store/index.js
+++ app/client/src/store/index.js
@@ -1,3 +1,21 @@
+/* ##
+## Copyright (c) 2020 Wind River Systems, Inc.
+##
+## Licensed under the Apache License, Version 2.0 (the "License");
+## you may not use this file except in compliance with the License.
+## You may obtain a copy of the License at:
+##       http://www.apache.org/licenses/LICENSE-2.0
+## Unless required by applicable law or agreed to in writing, software  distributed
+## under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES
+## OR CONDITIONS OF ANY KIND, either express or implied.
+
+Name:     store/project-submit/index.js
+Purpose: A vuex module to handle to project submission data.
+Methods:
+  * This file tells vuex to use the module defined in './project-submit'.
+
+## */
+
 import Vue from 'vue'
 import Vuex from 'vuex'
 
