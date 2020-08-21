313593605c4e3c546e247e8aed2b64f11fc8e797
Andrew Chen <Andrew.Chen@windriver.com>
2020-08-20 17:52:30 -0700

    Create empty layout and make error404 a page
    Signed-off-by: Andrew Chen <Andrew.Chen@windriver.com>

---
+++ app/client/src/layouts/EmptyLayout.vue
@@ -0,0 +1,21 @@
+<!-- //This layout is empty. In other wordss, it has no header, footer, left-side drawer, or right-side drawer. QHeader
+// I want a QFooter
+// I want a left-side QDrawer
+// I want a right-side QDrawer
+ -->
+<template>
+  <q-layout view="hHh lpR fFf">
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    }
+  }
+}
+</script>
--- app/client/src/router/routes.js
+++ app/client/src/router/routes.js
@@ -146,24 +146,19 @@ const routes = [
       }
     ]
   }
-  // {
-  //   path: '/inject',
-  //   component: () => import('layouts/BoundlessLayout.vue'),
-  //   children: [
-  //     {
-  //       path: '',
-  //       component: () => import('pages/util/Injection.vue')
-  //     }
-  //   ]
-  // }
 ]
 
 // Always leave this as last one
 if (process.env.MODE !== 'ssr') {
   routes.push({
     path: '*',
-    component: () => import('pages/Error404.vue')
+    component: () => import('layouts/EmptyLayout.vue'),
+    children: [
+      {
+        path: '',
+        component: () => import('pages/Error404.vue')
+      }
+    ]
   })
 }
-
 export default routes
