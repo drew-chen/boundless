57a0a2771bfe1d0628e6ac9ef537060f2a1d0778
Andrew Chen <Andrew.Chen@windriver.com>
2020-10-19 10:44:45 -0700

    Create load projects  by date order func

--- app/client/src/components/Manage/Shared/AdminTable.vue
+++ app/client/src/components/Manage/Shared/AdminTable.vue
@@ -424,13 +424,23 @@ export default {
         }
       }
     },
+    async loadRowsByDate (ascending) {
+      const docRef = await this.db.collection(`${this.rowType}s`).doc('ToC')
+      let docs
+      if (ascending) {
+        docs = await docRef.orderBy('created').limit(3).get()
+      } else {
+        docs = await docRef.orderBy('created', 'desc').get()
+      }
+      return docs
+    },
     /**
      * Load all the projects, challenges, or users from the ToC for the admin
      * console.
      */
     loadAllRows: async function () {
       try {
-        const doc = await this.db.collection(`${this.rowType}s`).doc('ToC').get()
+        const doc = this.loadRowsByDate(true)
 
         if (doc.exists) {
           const tocAllRowData = doc.data()
