22a01e308de8dd26110119c4afd3fdf7773e3815
Andrew Chen <Andrew.Chen@windriver.com>
2020-11-08 15:00:52 -0800

    Update new action documentation

--- app/client/src/store/project-display/actions.js
+++ app/client/src/store/project-display/actions.js
@@ -1,3 +1,30 @@
+/* ##
+## Copyright (c) 2020 Wind River Systems, Inc.
+##
+## Licensed under the Apache License, Version 2.0 (the "License");
+## you may not use this file except in compliance with the License.
+## You may obtain a copy of the License at:
+##       http://www.apache.org/licenses/LICENSE-2.0
+## Unless required by applicable law or agreed to in writing, software  distributed
+## under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES
+## OR CONDITIONS OF ANY KIND, either express or implied.
+
+Name:     store/project-display/actions.js
+Purpose:
+
+  Actions for the project-display Vuex module. Methods here are
+  responsible loading in info needed to display projects. This module
+  is also to help performance. Database calls can be loaded and finished
+  before they are needed by the project listing page. This is useful
+  since most users will eventually visit the project listing page.
+
+Methods:
+
+  Using the database reference from project-submit, various calls to the database
+  are made. These calls are stored in the state.
+
+## */
+
 import callDependingOnBackend from '../../store/callDependingOnBackend'
 
 /**
@@ -7,11 +34,12 @@ import callDependingOnBackend from '../../store/callDependingOnBackend'
  *   store instance.
  */
 export async function initStoreProjectSubmit (context) {
-  callDependingOnBackend(context, loadUserListFirebase)
+  callDependingOnBackend(context, loadProjectTocPromise)
+  callDependingOnBackend(context, loadUserTocPromise)
 }
 
 /**
- * Loads the table of contents for users.
+ * Loads the table of contents for projects.
  *
  * @param {Object} context Exposes the same set of methods/properties as the
  *   store instance.
@@ -20,7 +48,7 @@ export async function initStoreProjectSubmit (context) {
  */
 async function loadProjectTocPromise ({ commit, getters }) {
   const tocPromise = getters.db.collection('projects').doc('ToC').get()
-  commit('setTocPromise', tocPromise)
+  commit('setProjectTocPromise', tocPromise)
 }
 
 /**
@@ -32,6 +60,6 @@ async function loadProjectTocPromise ({ commit, getters }) {
  * @param {Object} context.getters Gives access to state.
  */
 async function loadUserTocPromise ({ commit, getters }) {
-  const tocPromise = getters.db.collection('projects').doc('ToC').get()
-  commit('setTocPromise', tocPromise)
+  const tocPromise = getters.db.collection('users').doc('ToC').get()
+  commit('setProjectTocPromise', tocPromise)
 }
