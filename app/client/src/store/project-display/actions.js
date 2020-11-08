db3af0b53cce0029700da5ef11212ef84abfced0
Andrew Chen <Andrew.Chen@windriver.com>
2020-11-08 14:42:19 -0800

    Create new Vuex module for project display

---
+++ app/client/src/store/project-display/actions.js
@@ -0,0 +1,37 @@
+/**
+ * Initializes state in this Vuex module.
+ *
+ * @param {Object} context Exposes the same set of methods/properties as the
+ *   store instance.
+ */
+export async function initStoreProjectSubmit (context) {
+  await callDependingOnBackend(context, loadFireRefs)
+  await loadConfig(context)
+  await loadUserList(context)
+}
+
+/**
+ * Loads the table of contents for users.
+ *
+ * @param {Object} context Exposes the same set of methods/properties as the
+ *   store instance.
+ * @param {Object} context.commit Allows this action to commit mutations
+ * @param {Object} context.getters Gives access to state.
+ */
+async function loadProjectTocPromise ({ commit, getters }) {
+  const tocPromise = getters.db.collection('projects').doc('ToC').get()
+  commit('setTocPromise', tocPromise)
+}
+
+/**
+ * Loads the table of contents for users.
+ *
+ * @param {Object} context Exposes the same set of methods/properties as the
+ *   store instance.
+ * @param {Object} context.commit Allows this action to commit mutations
+ * @param {Object} context.getters Gives access to state.
+ */
+async function loadUserTocPromise ({ commit, getters }) {
+  const tocPromise = getters.db.collection('projects').doc('ToC').get()
+  commit('setTocPromise', tocPromise)
+}
